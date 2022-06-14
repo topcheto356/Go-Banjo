0f8992c233308e4cfc3ed7a5906ccb5e56636b06
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-06-14 23:56:59 +0300

    created server folder

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
+//
 router.post('/signup', authController.signUp);
 router.post('/login', authController.login);
 
@@ -25,14 +26,14 @@ router.delete('/deleteMe', userController.deleteMe);
 router.use(authController.restrictTo('admin'));
 
 router
-	.route('/')
-	.get(userController.getAllUsers)
-	.post(userController.createUser);
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
 
 router
-	.route('/:id')
-	.get(userController.getUser)
-	.patch(userController.updateUser)
-	.delete(userController.deleteUser);
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
