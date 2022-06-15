e2e7c786e4f3b38e7f3bc2e581e4884cb9d9d1cf
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-06-16 02:33:32 +0300

    cleanup

--- client/go-banjo/src/App.js
+++ client/go-banjo/src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from 'react';
 
+import Naviagtion from './components/Naviagtion/Naviagtion.js';
 import Header from './components/Header/Header.js';
 
 function App() {
     return (
         <Fragment>
+            <Naviagtion></Naviagtion>
             <Header></Header>
         </Fragment>
     );
