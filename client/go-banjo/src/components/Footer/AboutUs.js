0921ab0544d9be7ac8b472309e1f3602933a9572
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-16 03:43:54 +0300

    footer- about us

---
+++ client/go-banjo/src/components/Footer/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';import info from './Footer';
+
+const AboutUs = (props) => {
+	return (
+		<div className={props.className}>
+			<p>Adress:</p>
+			<p>blablablabla</p>
+		</div>
+	);
+};
+
+export default AboutUs;
