de1feddc005ef897c29b95925b677c9c363dd6a7
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-06-16 15:20:01 +0300

    added react router

--- client/go-banjo/src/App.js
+++ client/go-banjo/src/App.js
@@ -3,14 +3,19 @@ import Naviagtion from './components/Naviagtion/Naviagtion.js';
 import Header from './components/Header/Header.js';
 import Footer from './components/Footer/Footer.js';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
-	return (
-		<Fragment>
-			<Naviagtion></Naviagtion>
-			<Header></Header>
-			<Footer></Footer>
-		</Fragment>
-	);
+    return (
+        <Fragment>
+            <Router>
+                <Naviagtion></Naviagtion>
+                <Switch>
+                    <Header></Header>
+                </Switch>
+                <Footer></Footer>
+            </Router>
+        </Fragment>
+    );
 }
 
 export default App;
--- client/go-banjo/src/components/Naviagtion/Naviagtion.js
+++ client/go-banjo/src/components/Naviagtion/Naviagtion.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 const Naviagtion = () => {
     return (
         <nav className='nav'>
             <h1 className='nav__logo'>Go-Banjo</h1>
             <ul className='nav__links'>
-                <li className='nav__item'>Houses</li>
-                <li className='nav__item'>Create AD</li>
-                <li className='nav__item login-btn'>Register</li>
+                <li className='nav__item'>
+                    <Link to={'/houses'} className='nav__link'>
+                        Houses
+                    </Link>
+                </li>
+                <li className='nav__item'>
+                    <Link to={'/ads'} className='nav__link'>
+                        Create AD
+                    </Link>
+                </li>
+                <li className='nav__item login-btn'>
+                    <Link to={'/register'} className='nav__link'>
+                        Register
+                    </Link>
+                </li>
             </ul>
         </nav>
     );
