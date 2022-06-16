d4a7416d17a1a2c946aa1252446489cb387e8032
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-16 03:43:15 +0300

    footer

--- client/go-banjo/src/App.js
+++ client/go-banjo/src/App.js
@@ -1,15 +1,16 @@
 import { Fragment } from 'react';
-
 import Naviagtion from './components/Naviagtion/Naviagtion.js';
 import Header from './components/Header/Header.js';
+import Footer from './components/Footer/Footer.js';
 
 function App() {
-    return (
-        <Fragment>
-            <Naviagtion></Naviagtion>
-            <Header></Header>
-        </Fragment>
-    );
+	return (
+		<Fragment>
+			<Naviagtion></Naviagtion>
+			<Header></Header>
+			<Footer></Footer>
+		</Fragment>
+	);
 }
 
 export default App;
---
+++ client/go-banjo/src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';import AboutUs from './AboutUs';
+import FollowUsOn from './FollowUsOn';
+
+const Footer = () => {
+	return (
+		<footer className="footer">
+			<main className="footer__main">
+				<div className="footer__main__section">
+					<img alt="icons" />
+				</div>
+				<AboutUs className="footer__main__section" />
+				<FollowUsOn className="footer__main__section" />
+				<div className="footer__main__section">Contact us</div>
+			</main>
+			<main className="footer__main">
+				<p className="footer__main__bottom">&copy; All rights reserved</p>
+			</main>
+		</footer>
+	);
+};
+
+export default Footer;
