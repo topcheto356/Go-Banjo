56c5503556833593c3c0ae2fdc05d0f9f19da296
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-16 03:39:27 +0300

    icon component

---
+++ client/go-banjo/src/components/Footer/Icons.js
@@ -0,0 +1,16 @@
+import React from 'react';import Icons from '../../img/icons.svg';
+import PropTypes from 'prop-types';
+
+const Icon = ({ name, color, size }) => (
+	<svg className={`icon icon-${name}`} fill={color} width={size} height={size}>
+		<use xlinkHref={`${Icons}#icon-${name}`} />
+	</svg>
+);
+
+Icon.propTypes = {
+	name: PropTypes.string.isRequired,
+	color: PropTypes.string,
+	size: PropTypes.number,
+};
+
+export default Icon;
