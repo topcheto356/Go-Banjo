d165540a36a0ac06edba590d7f7795946db9a49a
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-16 03:43:42 +0300

    footer - foolouson

---
+++ client/go-banjo/src/components/Footer/FollowUsOn.js
@@ -0,0 +1,14 @@
+import React from 'react';import Icons from './Icons';
+
+const FollowUsOn = (props) => {
+	return (
+		<div className={props.className}>
+			<p className="section__title">FollowUsOn</p>
+			<Icons name="map-pin" color="white" size={100} />
+			<p>Address:</p>
+			<p>blablablablabla</p>
+		</div>
+	);
+};
+
+export default FollowUsOn;
