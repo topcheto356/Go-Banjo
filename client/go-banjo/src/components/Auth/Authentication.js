308265352607bdb64f021e9f892f515c1530b5be
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-21 02:04:26 +0300

    add register form

--- client/go-banjo/src/components/Auth/Authentication.js
+++ client/go-banjo/src/components/Auth/Authentication.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Login from './Login';
+import Register from './Register';
 
 const Authentication = () => {
 	return (
 		<section>
 			<Login />;
+			<Register />
 		</section>
 	);
 };
