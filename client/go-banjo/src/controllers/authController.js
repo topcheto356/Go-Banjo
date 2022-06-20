3d221d450f95cf0aaa24a0f1028a9110e59c19ee
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-21 02:04:38 +0300

    register controller

--- client/go-banjo/src/controllers/authController.js
+++ client/go-banjo/src/controllers/authController.js
@@ -1,17 +1,39 @@
-import axios from 'axios';import catchAsync from '../utils/catchAsync';
+import axios from 'axios';
+import catchAsync from '../utils/catchAsync';
 
-export const login = async (formData) => {
+export const login = async (loginData) => {
 	const config = {
 		headers: {
 			'Content-Type': 'application/json',
 		},
 	};
 
-	const body = JSON.stringify(formData);
+	const body = JSON.stringify(loginData);
 
 	try {
 		const res = await axios.post(
 			'http://localhost:8000/api/users/login',
+			body,
+			config
+		);
+		console.log(res.data);
+	} catch (err) {
+		console.log(3);
+	}
+};
+
+export const register = async (registerData) => {
+	const config = {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
+
+	const body = JSON.stringify(registerData);
+
+	try {
+		const res = await axios.post(
+			'http://localhost:8000/api/users/signup',
 			body,
 			config
 		);
