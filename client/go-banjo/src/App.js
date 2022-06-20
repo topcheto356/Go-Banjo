4152305f02aa052fb7ff55922a77b993d1dd62e7
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-21 00:47:29 +0300

    add register button to nav

--- client/go-banjo/src/App.js
+++ client/go-banjo/src/App.js
@@ -4,18 +4,20 @@ import Header from './components/Header/Header.js';
 import Footer from './components/Footer/Footer.js';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Authentication from './components/Auth/Authentication.js';
 function App() {
-    return (
-        <Fragment>
-            <Router>
-                <Naviagtion></Naviagtion>
-                <Switch>
-                    <Route exact path='/' component={Header} />
-                </Switch>
-                <Footer></Footer>
-            </Router>
-        </Fragment>
-    );
+	return (
+		<Fragment>
+			<Router>
+				<Naviagtion></Naviagtion>
+				<Switch>
+					<Route exact path="/" component={Header} />
+					<Route exact path="/register" component={Authentication} />
+				</Switch>
+				<Footer></Footer>
+			</Router>
+		</Fragment>
+	);
 }
 
 export default App;
