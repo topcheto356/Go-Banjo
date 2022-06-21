635055cb41f278eed0b9d6d43154e52886f6a577
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-06-21 16:16:08 +0300

    added switch button between register and login

--- client/go-banjo/src/components/Auth/Authentication.js
+++ client/go-banjo/src/components/Auth/Authentication.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useState } from 'react';
 import Login from './Login';
 import Register from './Register';
 
 const Authentication = () => {
+    const [register, setRegister] = useState(false);
+
+    const switchHandler = (e) => {
+        console.log(register);
+        setRegister(!register);
+    };
+
     return (
-        <section>
-            <Login />
-            <Register />
-        </section>
+        <Fragment>
+            <section className='authentication'>
+                <div className='button-wrapper'>
+                    <button
+                        className='btn-white auth-btn-position'
+                        onClick={switchHandler}
+                    >
+                        Switch To {register ? 'Login' : 'Register'}
+                    </button>
+                </div>
+                {register ? <Register /> : <Login />}
+            </section>
+        </Fragment>
     );
 };
 
