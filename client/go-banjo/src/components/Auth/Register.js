8b4562cfe6bdb5f582a67909128b4959a56e5438
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-22 15:04:03 +0300

    add style to input
    valid or invalid

--- client/go-banjo/src/components/Auth/Register.js
+++ client/go-banjo/src/components/Auth/Register.js
@@ -5,6 +5,12 @@ import {
 	validateEnteredPassword,
 } from '../../controllers/validateData';
 const Register = () => {
+	let firstNameStyle,
+		lastNameStyle,
+		emailStyle,
+		passwordStyle,
+		confirmPasswordStyle;
+
 	const [enteredFirstName, setEnteredFirstName] = useState('');
 	const [enteredLastName, setEnteredLastName] = useState('');
 	const [enteredEmail, setEnteredEmail] = useState('');
@@ -14,35 +20,35 @@ const Register = () => {
 	const enteredFirstNameHandler = (event) => {
 		const { style, name } = validateEnteredName(event);
 
-		const firstNameStyle = style;
+		firstNameStyle = style;
 
 		setEnteredFirstName(name);
 	};
 	const enteredLastNameHandler = (event) => {
 		const { style, name } = validateEnteredName(event);
 
-		const lastNameStyle = style;
+		lastNameStyle = style;
 
 		setEnteredLastName(name);
 	};
 	const enteredEmailHandler = (event) => {
 		const { style, email } = validateEnteredName(event);
 
-		const emailStyle = style;
+		emailStyle = style;
 
 		setEnteredEmail(email);
 	};
 	const enteredPasswordHandler = (event) => {
 		const { style, password } = validateEnteredPassword(event);
 
-		const passwordStyle = style;
+		passwordStyle = style;
 
 		setEnteredPassword(password);
 	};
 	const enteredConfirmPasswordHandler = (event, enteredPassword) => {
 		const { style, confirmPassword } = validateEnteredPassword(event);
 
-		const confirmPasswordStyle = style;
+		confirmPasswordStyle = style;
 
 		setEnteredConfirmPassword(confirmPassword);
 	};
@@ -66,7 +72,7 @@ const Register = () => {
 					<div className="auth__control">
 						<label className="auth__label">First Name</label>
 						<input
-							className="auth__input"
+							className={`auth__input ${firstNameStyle}`}
 							type="text"
 							value={enteredFirstName}
 							onChange={enteredFirstNameHandler}
@@ -75,7 +81,7 @@ const Register = () => {
 					<div className="auth__control">
 						<label className="auth__label">Last Name</label>
 						<input
-							className="auth__input"
+							className={`auth__input ${lastNameStyle}`}
 							type="text"
 							value={enteredLastName}
 							onChange={enteredLastNameHandler}
@@ -84,7 +90,7 @@ const Register = () => {
 					<div className="auth__control">
 						<label className="auth__label">Email</label>
 						<input
-							className="auth__input"
+							className={`auth__input ${emailStyle}`}
 							type="text"
 							value={enteredEmail}
 							onChange={enteredEmailHandler}
@@ -93,7 +99,7 @@ const Register = () => {
 					<div className="auth__control">
 						<label className="auth__label">Password</label>
 						<input
-							className="auth__input"
+							className={`auth__input ${passwordStyle}`}
 							type="text"
 							value={enteredPassword}
 							onChange={enteredPasswordHandler}
@@ -102,7 +108,7 @@ const Register = () => {
 					<div className="auth__control">
 						<label className="auth__label">Confirm Password</label>
 						<input
-							className="auth__input"
+							className={`auth__input ${confirmPasswordStyle}`}
 							type="text"
 							value={enteredConfirmPassword}
 							onChange={enteredConfirmPasswordHandler}
