d5f9831698e802e19857bb7e3520ce841a584436
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-06-24 13:10:54 +0300

    fixed path

--- client/go-banjo/src/controllers/inputFields/userFields.js
+++ client/go-banjo/src/controllers/inputFields/userFields.js
@@ -1,4 +1,4 @@
-import validateUserData from '../vatidateData/validateUser';
+import validateUserData from '../VatidateData/validateUser.js';
 
 /*
     {
@@ -11,42 +11,44 @@ import validateUserData from '../vatidateData/validateUser';
 */
 
 const userFields = [
-	{
-		name: 'First Name',
-		type: 'text',
-		validate: validateUserData.userName,
-		errMessage: 'First name must be less than 30 characters and only letters',
-		field: 'firstName',
-	},
-	{
-		name: 'Last Name',
-		type: 'text',
-		validate: validateUserData.userName,
-		errMessage: 'Last name must be less than 30 characters and only letters',
-		field: 'lastName',
-	},
-	{
-		name: 'Email',
-		type: 'text',
-		validate: validateUserData.email,
-		errMessage: 'Enter valid email',
-		field: 'email',
-	},
-	{
-		name: 'Password',
-		type: 'text',
-		validate: validateUserData.password,
-		errMessage: 'A password must be minimum 8 characters',
-		field: 'password',
-	},
-	{
-		name: 'Confirm Password',
-		type: 'text',
-		validate: validateUserData.confirmPassword,
-		errMessage: 'Passwords are NOT the same',
-		field: 'passwordConfirm',
-		enteredPassword: '',
-	},
+    {
+        name: 'First Name',
+        type: 'text',
+        validate: validateUserData.userName,
+        errMessage:
+            'First name must be less than 30 characters and only letters',
+        field: 'firstName',
+    },
+    {
+        name: 'Last Name',
+        type: 'text',
+        validate: validateUserData.userName,
+        errMessage:
+            'Last name must be less than 30 characters and only letters',
+        field: 'lastName',
+    },
+    {
+        name: 'Email',
+        type: 'text',
+        validate: validateUserData.email,
+        errMessage: 'Enter valid email',
+        field: 'email',
+    },
+    {
+        name: 'Password',
+        type: 'text',
+        validate: validateUserData.password,
+        errMessage: 'A password must be minimum 8 characters',
+        field: 'password',
+    },
+    {
+        name: 'Confirm Password',
+        type: 'text',
+        validate: validateUserData.confirmPassword,
+        errMessage: 'Passwords are NOT the same',
+        field: 'passwordConfirm',
+        enteredPassword: '',
+    },
 ];
 
 export default userFields;
