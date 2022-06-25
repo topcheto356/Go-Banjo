a794d4a786fb7a1e3cb5f620aa2d3cf44db56c1d
topcheto356 <miro.kurtev27@gmail.com>
2022-06-25 21:29:13 +0300

    resize uploaded images

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,23 +1,27 @@
 const multer = require('multer');
+const sharp = require('sharp');
 
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
-const multerStorage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, '../client/go-banjo/src/img/users');
-	},
-	filename: (req, file, cb) => {
-		// user-user.id-currentTimeStamp
-		// user-ms8f3bcw2341d-1324612341.jpeg
+// const multerStorage = multer.diskStorage({
+// 	destination: (req, file, cb) => {
+// 		cb(null, '../client/go-banjo/src/img/users');
+// 	},
+// 	filename: (req, file, cb) => {
+// 		// user-user.id-currentTimeStamp
+// 		// user-ms8f3bcw2341d-1324612341.jpeg
 
-		const ext = file.mimetype.split('/')[1];
+// 		const ext = file.mimetype.split('/')[1];
 
-		cb(null, `user-${req.user._doc._id}-${Date.now()}.${ext}`);
-	},
-});
+// 		cb(null, `user-${req.user._doc._id}-${Date.now()}.${ext}`);
+// 	},
+// });
+
+//store image as buffer
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
 	if (file.mimetype.startsWith('image')) {
@@ -33,6 +37,20 @@ const upload = multer({
 });
 
 exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = (req, res, next) => {
+	if (!req.file) return next();
+
+	req.file.filename = `user-${req.user._doc._id}-${Date.now()}.jpeg`;
+
+	sharp(req.file.buffer)
+		.resize(500, 500)
+		.toFormat('jpeg')
+		.jpeg({ quality: 90 })
+		.toFile(`../client/go-banjo/src/img/users/${req.file.filename}`);
+
+	next();
+};
 
 //filter the updated data
 const filterObj = (obj, allowedFields) => {
