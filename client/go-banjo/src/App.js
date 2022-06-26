01964ca67086cc26df85a58e0476e5190bf73e86
topcheto356 <miro.kurtev27@gmail.com>
2022-06-26 15:53:34 +0300

    update user

--- client/go-banjo/src/App.js
+++ client/go-banjo/src/App.js
@@ -10,28 +10,30 @@ import { useEffect } from 'react';
 import { loadUser } from './controllers/authController.js';
 import Account from './components/Account/Account.js';
 import Houses from './components/Houses/Houses.js';
+import UpdateUser from './components/Account/UpdateUser.js';
 function App() {
-    useEffect(() => {
-        if (localStorage.getItem('token')) {
-            loadUser(localStorage.getItem('token'));
-        }
-    }, []);
+	useEffect(() => {
+		if (localStorage.getItem('token')) {
+			loadUser(localStorage.getItem('token'));
+		}
+	}, []);
 
-    return (
-        <Fragment>
-            <Router>
-                <Naviagtion></Naviagtion>
-                <Switch>
-                    <Route exact path='/' component={Header} />
-                    <Route exact path='/auth' component={Authentication} />
-                    <Route exact path='/me' component={Account} />
-                    <Route exact path='/ads' component={CreateAd} />
-                    <Route exact path='/houses' component={Houses} />
-                </Switch>
-                {/* <Footer></Footer> */}
-            </Router>
-        </Fragment>
-    );
+	return (
+		<Fragment>
+			<Router>
+				<Naviagtion></Naviagtion>
+				<Switch>
+					<Route exact path='/' component={Header} />
+					<Route exact path='/auth' component={Authentication} />
+					<Route exact path='/me' component={Account} />
+					<Route exact path='/updateMe' component={UpdateUser} />
+					<Route exact path='/ads' component={CreateAd} />
+					<Route exact path='/houses' component={Houses} />
+				</Switch>
+				{/* <Footer></Footer> */}
+			</Router>
+		</Fragment>
+	);
 }
 
 export default App;
