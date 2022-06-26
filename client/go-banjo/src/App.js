1910628f1e24ae6aed425b51d5ab55a60925ff5f
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-06-26 23:39:39 +0300

    app file

--- client/go-banjo/src/App.js
+++ client/go-banjo/src/App.js
@@ -11,29 +11,31 @@ import { loadUser } from './controllers/authController.js';
 import Account from './components/Account/Account.js';
 import Houses from './components/Houses/Houses.js';
 import UpdateUser from './components/Account/UpdateUser.js';
+import House from './components/House/House.js';
 function App() {
-	useEffect(() => {
-		if (localStorage.getItem('token')) {
-			loadUser(localStorage.getItem('token'));
-		}
-	}, []);
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            loadUser(localStorage.getItem('token'));
+        }
+    }, []);
 
-	return (
-		<Fragment>
-			<Router>
-				<Naviagtion></Naviagtion>
-				<Switch>
-					<Route exact path='/' component={Header} />
-					<Route exact path='/auth' component={Authentication} />
-					<Route exact path='/me' component={Account} />
-					<Route exact path='/updateMe' component={UpdateUser} />
-					<Route exact path='/ads' component={CreateAd} />
-					<Route exact path='/houses' component={Houses} />
-				</Switch>
-				{/* <Footer></Footer> */}
-			</Router>
-		</Fragment>
-	);
+    return (
+        <Fragment>
+            <Router>
+                <Naviagtion></Naviagtion>
+                <Switch>
+                    <Route exact path='/' component={Header} />
+                    <Route exact path='/auth' component={Authentication} />
+                    <Route exact path='/me' component={Account} />
+                    <Route exact path='/updateMe' component={UpdateUser} />
+                    <Route exact path='/ads' component={CreateAd} />
+                    <Route exact path='/houses' component={Houses} />
+                    <Route exact path='/houses/:id' component={House} />
+                </Switch>
+                {/* <Footer></Footer> */}
+            </Router>
+        </Fragment>
+    );
 }
 
 export default App;
