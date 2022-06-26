9478b8e2c80ebf4e660b379717ad53af0621724b
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-06-26 16:53:54 +0300

    tabulation

--- server/controllers/houseController.js
+++ server/controllers/houseController.js
@@ -2,6 +2,7 @@ const multer = require('multer');
 const sharp = require('sharp');
 
 const House = require('../models/houseModel');
+const AppError = require('../utils/appError.js');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
@@ -9,36 +10,36 @@ const factory = require('./handlerFactory');
 const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
-	if (file.mimetype.startsWith('image')) {
-		cb(null, true);
-	} else {
-		cb(new AppError('Not image, please upload only images', 400), false);
-	}
+    if (file.mimetype.startsWith('image')) {
+        cb(null, true);
+    } else {
+        cb(new AppError('Not image, please upload only images', 400), false);
+    }
 };
 
 const upload = multer({
-	storage: multerStorage,
-	fileFilter: multerFilter,
+    storage: multerStorage,
+    fileFilter: multerFilter,
 });
 
 exports.uploadHouseImages = upload.fields([
-	{ name: 'imageCover', maxCount: 1 },
-	{ name: 'images', maxCount: 5 },
+    { name: 'imageCover', maxCount: 1 },
+    { name: 'images', maxCount: 5 },
 ]);
 
 exports.resizeHouseImages = catchAsync(async (req, res, next) => {
-	if (!req.files.imageCover || !req.files.images) return next();
+    if (!req.files.imageCover || !req.files.images) return next();
 
-	// cover image
-	req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+    // cover image
+    req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
 
-	await sharp(req.files.imageCover[0].buffer)
-		.resize(2000, 1333)
-		.toFormat('jpeg')
-		.jpeg({ quality: 90 })
-		.toFile(`../client/go-banjo/src/img/houses/${req.body.imageCover}`);
+    await sharp(req.files.imageCover[0].buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`../client/go-banjo/src/img/houses/${req.body.imageCover}`);
 
-	next();
+    next();
 });
 
 // Create new guest house
@@ -58,9 +59,9 @@ exports.updateHouse = factory.updateOne(House);
 
 // Get top 5 houses
 exports.topHouses = (req, res, next) => {
-	req.query.limit = '5';
-	req.query.sort = '-ratingAverage,price';
-	req.query.fields = 'name,price,ratingsAverage';
+    req.query.limit = '5';
+    req.query.sort = '-ratingAverage,price';
+    req.query.fields = 'name,price,ratingsAverage';
 
-	next();
+    next();
 };
