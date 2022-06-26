766bf3ffdef80827cca92d2602459ee989c45156
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-06-26 15:26:09 +0300

    minor style change

--- client/go-banjo/src/components/Header/Features.js
+++ client/go-banjo/src/components/Header/Features.js
@@ -2,50 +2,40 @@ import React from 'react';
 import Icon from '../UI/Icon.js';
 
 const Features = () => {
-	return (
-		<section className='features'>
-			<div className='feature'>
-				<Icon name='home'></Icon>
-				<div className='feature__text-container'>
-					<h3 className='heading-tertiary'>Epic</h3>
-					<p className='paragraph'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Reprehenderit vel veniam, incidunt nisi.
-					</p>
-				</div>
-			</div>
-			<div className='feature'>
-				<Icon name='home'></Icon>
-				<div className='feature__text-container'>
-					<h3 className='heading-tertiary'>Epic</h3>
-					<p className='paragraph'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Reprehenderit vel veniam, incidunt nisi.
-					</p>
-				</div>
-			</div>
-			<div className='feature'>
-				<Icon name='home'></Icon>
-				<div className='feature__text-container'>
-					<h3 className='heading-tertiary'>Epic</h3>
-					<p className='paragraph'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Reprehenderit vel veniam, incidunt nisi.
-					</p>
-				</div>
-			</div>
-			<div className='feature'>
-				<Icon name='home'></Icon>
-				<div className='feature__text-container'>
-					<h3 className='heading-tertiary'>Epic</h3>
-					<p className='paragraph'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Reprehenderit vel veniam, incidunt nisi.
-					</p>
-				</div>
-			</div>
-		</section>
-	);
+    return (
+        <section className='features'>
+            <div className='feature'>
+                <Icon name='home'></Icon>
+                <div className='feature__text-container'>
+                    <h3 className='heading-tertiary'>Epic</h3>
+                    <p className='paragraph'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Reprehenderit vel veniam, incidunt nisi.
+                    </p>
+                </div>
+            </div>
+            <div className='feature'>
+                <Icon name='home'></Icon>
+                <div className='feature__text-container'>
+                    <h3 className='heading-tertiary'>Epic</h3>
+                    <p className='paragraph'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Reprehenderit vel veniam, incidunt nisi.
+                    </p>
+                </div>
+            </div>
+            <div className='feature'>
+                <Icon name='home'></Icon>
+                <div className='feature__text-container'>
+                    <h3 className='heading-tertiary'>Epic</h3>
+                    <p className='paragraph'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Reprehenderit vel veniam, incidunt nisi.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Features;
