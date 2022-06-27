8c2b86c1c24761890d2b863c9b9e0420dad24de1
topcheto356 <miro.kurtev27@gmail.com>
2022-06-27 16:12:53 +0300

    fuck prettier

--- client/go-banjo/src/controllers/housesController.js
+++ client/go-banjo/src/controllers/housesController.js
@@ -2,68 +2,66 @@ import axios from 'axios';
 import catchAsync from '../utils/catchAsync';
 import store from '../store.js';
 import {
-    addHouse,
-    loadAllHouses,
-    loadHouse,
-    removeHouse,
+	addHouse,
+	loadAllHouses,
+	loadHouse,
+	removeHouse,
 } from '../slices/houseSlice.js';
 
 export const createHouse = async (house) => {
-    const config = {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    };
+	const config = {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
 
-    const body = JSON.stringify(house);
+	const body = JSON.stringify(house);
 
-    try {
-        const res = await axios.post(
-            'http://localhost:8000/api/houses',
-            body,
-            config
-        );
+	try {
+		const res = await axios.post(
+			'http://localhost:8000/api/houses',
+			body,
+			config
+		);
 
-        console.log(res);
+		console.log(res);
 
-        store.dispatch(addHouse(res.data));
-    } catch (err) {
-        console.log(3);
-    }
+		store.dispatch(addHouse(res.data));
+	} catch (err) {
+		console.log(3);
+	}
 };
 
 export const getAllHouses = async () => {
-    try {
-        const res = await axios.get('http://localhost:8000/api/houses');
+	try {
+		const res = await axios.get('http://localhost:8000/api/houses');
 
-        console.log(res);
+		console.log(res);
 
-        store.dispatch(loadAllHouses(res.data));
-    } catch (err) {
-        console.log(err);
-    }
+		store.dispatch(loadAllHouses(res.data));
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 export const deleteHouse = async (id) => {
-    try {
-        console.log(id);
-        const res = await axios.delete(
-            `http://localhost:8000/api/houses/${id}`
-        );
+	try {
+		console.log(id);
+		const res = await axios.delete(`http://localhost:8000/api/houses/${id}`);
 
-        console.log(res);
+		console.log(res);
 
-        store.dispatch(removeHouse(id));
-    } catch (err) {
-        console.log(3);
-    }
+		store.dispatch(removeHouse(id));
+	} catch (err) {
+		console.log(3);
+	}
 };
 
 export const getHouse = async (id) => {
-    try {
-        console.log(id);
-        const res = await axios.get(`http://localhost:8000/api/houses/${id}`);
+	try {
+		console.log(id);
+		const res = await axios.get(`http://localhost:8000/api/houses/${id}`);
 
-        store.dispatch(loadHouse(res.data));
-    } catch (err) {}
+		store.dispatch(loadHouse(res.data));
+	} catch (err) {}
 };
