c1867ad3fe407b4e2bf6439e769246a3401d7b4a
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-06-27 16:23:15 +0300

    created private route component

---
+++ client/go-banjo/src/components/routes/PrivateRoute.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect, Route } from 'react-router-dom';
+
+const PrivateRoute = ({ children, ...rest }) => {
+    const user = useSelector((state) => state.auth.user);
+
+    if (!user) {
+        return <Redirect to='/auth' />;
+    }
+
+    console.log(children);
+
+    return (
+        <Route
+            {...rest}
+            render={() =>
+                user.isAuthenticated ? children : <Redirect to='/auth' />
+            }
+        />
+    );
+};
+
+export default PrivateRoute;
