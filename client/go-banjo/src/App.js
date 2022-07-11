6e03d15b0c56cf2e47bad2118897273a3dc4e735
topcheto356 <miro.kurtev27@gmail.com>
2022-07-11 23:33:01 +0300

    remove update me route

--- client/go-banjo/src/App.js
+++ client/go-banjo/src/App.js
@@ -10,39 +10,33 @@ import { useEffect } from 'react';
 import { loadUser } from './controllers/authController.js';
 import Account from './components/Account/Account.js';
 import Houses from './components/Houses/Houses.js';
-import UpdateUser from './components/Account/UpdateUser.js';
 import House from './components/House/House.js';
 import PrivateRoute from './components/routes/PrivateRoute.js';
 import Error from './components/Error/Error.js';
 function App() {
-    useEffect(() => {
-        if (localStorage.getItem('token')) {
-            loadUser(localStorage.getItem('token'));
-        }
-    }, []);
+	useEffect(() => {
+		if (localStorage.getItem('token')) {
+			loadUser(localStorage.getItem('token'));
+		}
+	}, []);
 
-    return (
-        <Fragment>
-            <Router>
-                <Naviagtion></Naviagtion>
-                <Switch>
-                    <Route exact path='/' component={Header} />
-                    <Route exact path='/auth' component={Authentication} />
-                    <PrivateRoute exact path='/me' component={Account} />
-                    <PrivateRoute
-                        exact
-                        path='/updateMe'
-                        component={UpdateUser}
-                    />
-                    <PrivateRoute exact path='/ads' component={CreateAd} />
-                    <Route exact path='/houses' component={Houses} />
-                    <Route exact path='/houses/:id' component={House} />
-                    <Route path='' component={Error} />
-                </Switch>
-                {/* <Footer></Footer> */}
-            </Router>
-        </Fragment>
-    );
+	return (
+		<Fragment>
+			<Router>
+				<Naviagtion></Naviagtion>
+				<Switch>
+					<Route exact path='/' component={Header} />
+					<Route exact path='/auth' component={Authentication} />
+					<PrivateRoute exact path='/me' component={Account} />
+					<PrivateRoute exact path='/ads' component={CreateAd} />
+					<Route exact path='/houses' component={Houses} />
+					<Route exact path='/houses/:id' component={House} />
+					<Route path='' component={Error} />
+				</Switch>
+				{/* <Footer></Footer> */}
+			</Router>
+		</Fragment>
+	);
 }
 
 export default App;
