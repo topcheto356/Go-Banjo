8ccb724364575d48dd57211bad0d28c232495781
topcheto356 <miro.kurtev27@gmail.com>
2022-08-11 16:33:39 +0300

    hate white spaces

--- client/go-banjo/src/components/House/HouseContainer.js
+++ client/go-banjo/src/components/House/HouseContainer.js
@@ -4,66 +4,59 @@ import { useParams } from 'react-router-dom';
 import { getHouse } from '../../controllers/housesController.js';
 
 const HouseContainer = () => {
-    const [current, setCurrent] = useState(0);
-    let length = 0;
-    const { id } = useParams();
+	const [current, setCurrent] = useState(0);
+	let length = 0;
+	const { id } = useParams();
 
-    useEffect(() => {
-        const awaitHouse = async () => {
-            await getHouse(id);
-        };
+	useEffect(() => {
+		const awaitHouse = async () => {
+			await getHouse(id);
+		};
 
-        awaitHouse();
-    }, [id]);
+		awaitHouse();
+	}, [id]);
 
-    const house = useSelector((state) => state.house.house);
+	const house = useSelector((state) => state.house.house);
 
-    if (!house) {
-        return <div>Loading</div>;
-    }
+	if (!house) {
+		return <div>Loading</div>;
+	}
 
-    length = house.images.length;
+	length = house.images.length;
 
-    const nextImage = () => {
-        setCurrent(current === length - 1 ? 0 : current + 1);
-    };
+	const nextImage = () => {
+		setCurrent(current === length - 1 ? 0 : current + 1);
+	};
 
-    const prevImage = () => {
-        setCurrent(current === 0 ? length - 1 : current - 1);
-    };
+	const prevImage = () => {
+		setCurrent(current === 0 ? length - 1 : current - 1);
+	};
 
-    return (
-        <div className='house__container'>
-            <div className='house__image'>
-                <span className='left-arrow' onClick={prevImage}>
-                    &larr;
-                </span>
-                <span className='right-arrow' onClick={nextImage}>
-                    &rarr;
-                </span>
-                {house.images.map((image, index) => {
-                    return (
-                        <div
-                            className={
-                                index === current ? 'slide active' : 'slide'
-                            }
-                            key={index}
-                        >
-                            {index === current && (
-                                <img
-                                    src={image}
-                                    alt=''
-                                    className='house__image-item'
-                                />
-                            )}
-                        </div>
-                    );
-                })}
-            </div>
-
-            <div className='house__info'></div>
-        </div>
-    );
+	return (
+		<div className='house__container'>
+			<div className='house__image'>
+				<span className='left-arrow' onClick={prevImage}>
+					&larr;
+				</span>
+				<span className='right-arrow' onClick={nextImage}>
+					&rarr;
+				</span>
+				{house.images.map((image, index) => {
+					return (
+						<div
+							className={index === current ? 'slide active' : 'slide'}
+							key={index}
+						>
+							{index === current && (
+								<img src={image} alt='' className='house__image-item' />
+							)}
+						</div>
+					);
+				})}
+			</div>
+			<div className='house__info'></div>
+		</div>
+	);
 };
 
 export default HouseContainer;
